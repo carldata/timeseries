1931e874780849edbc77abd85f38fa4f395c304f
klangner <klangner@gmail.com>
2017-03-24 10:52:02 +0100

    Index based on DateTime instead of String

--- src/main/scala/com/carl/ts/Series.scala
+++ src/main/scala/com/carl/ts/Series.scala
@@ -1,17 +1,20 @@
 package com.carl.ts
 
+import java.time.LocalDateTime
+
+
 object Series {
 
   /** Create TimeSeries from data in rows format */
-  def fromRows(rows: Seq[(String, Double)]): Series = {
+  def fromRows(rows: Seq[(LocalDateTime, Double)]): Series = {
     val (idx, vs) = rows.unzip
     new Series(idx, vs)
   }
 }
 
-class Series(idx: Seq[String], vs: Seq[Double]) {
+class Series(idx: Seq[LocalDateTime], vs: Seq[Double]) {
 
-  private val index: Seq[String] = idx
+  private val index: Seq[LocalDateTime] = idx
   private val values: Seq[Double] = vs
 
   def length: Int = index.length
--- src/test/scala/com/carl/ts/SeriesTest.scala
+++ src/test/scala/com/carl/ts/SeriesTest.scala
@@ -1,17 +1,40 @@
 package com.carl.ts
 
+import java.time.LocalDateTime
+
 import org.scalatest._
 
 
 class SeriesTest extends FlatSpec with Matchers {
 
   "Series" should "have length equal to its index" in {
-    val series = new Series(Seq("2015", "2016", "2017"), Seq(1.2))
+    val now = LocalDateTime.now()
+    val series = new Series(
+      Seq(now, now.plusMinutes(1), now.plusMinutes(2)),
+      Seq(1.2))
     series.length shouldBe 3
   }
 
   it should "be build from the rows" in {
-    val series = Series.fromRows(Seq(("2015", 1), ("2016", 3.4), ("2017", 5.6)))
+    val now = LocalDateTime.now()
+    val series = Series.fromRows(Seq((now, 1), (now.plusMinutes(1), 3.4), (now.plusMinutes(2), 5.6)))
     series.length shouldBe 3
   }
+
+//  it "element by index" $ do
+//  let xs = TS.tsSeries [1..] [10.0, 1.2, 32.4, 0.65, 11.0]
+//  fmap TS.dpValue (TS.elemAt 2 xs) `shouldBe` Just 32.4
+//
+//  it "map over series" $ do
+//  let xs = TS.tsSeries [1..] [10.0, 1.2, 32.4, 0.65, 11.0]
+//  let ys = fmap (+ 2) xs
+//    TS.values ys `shouldBe` [12.0, 3.2, 34.4, 2.65, 13.0]
+//
+//  it "maximum value" $ do
+//  let xs = TS.tsSeries [1..] [10.0, 1.2, 32.4, 0.65, 11.0]
+//  maximum xs `shouldBe` 32.4
+//
+//  it "minimum value" $ do
+//  let xs = TS.tsSeries [1..5] [10.0, 1.2, 32.4, 0.65, 11.0]
+//  minimum xs `shouldBe` 0.65
 }
