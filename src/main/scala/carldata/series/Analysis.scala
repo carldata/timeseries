f98f154bdd68b2274ea9c8b09db4e80304a744bd
sharpcoding <sharp.coding@gmail.com>
2017-09-22 16:03:09 +0200

    the second attempt to implement findSessions() - using foldLeft() and zip()

--- src/main/scala/carldata/series/Analysis.scala
+++ src/main/scala/carldata/series/Analysis.scala
@@ -1,38 +1,59 @@
 package carldata.series
 
-import scala.annotation.tailrec
+import java.time.LocalDateTime
+
 
 object Analysis {
   implicit class SeriesAnalysis[V](val ts: TimeSeries[V])(implicit num: Numeric[V]) {
 
     def findSessions: Seq[Session] = {
-      @tailrec def traverseTimeSeries[V](ts: TimeSeries[V],
-                                         sessionStartIndex: Int,
-                                         headIndex: Int,
-                                         detectedSession: Boolean,
-                                         result: Seq[Session]): Seq[Session] = {
-        ts match {
-          case TimeSeries(_ +: idxTail, dsHead +: dsTail) => {
-            if (dsHead != 0) {
-              traverseTimeSeries(TimeSeries(idxTail, dsTail),
-                sessionStartIndex,
-                headIndex + 1,
-                true,
-                result)
-            }
-            else {
-              traverseTimeSeries(TimeSeries(idxTail, dsTail),
-                headIndex + 1,
-                headIndex + 1,
-                false,
-                if (detectedSession) result :+ Session(sessionStartIndex, headIndex - 1) else result)
-            }
-          }
-          case _ => if (detectedSession) result :+ Session(sessionStartIndex, headIndex - 1) else result
-        }
+      /**
+        * Auxiliary class for storing value of a TimeSeries and its associated index in TimeSeries (where it can be found)
+        */
+      case class IndexValue(index: Int, value: V)
+
+      /**
+        * First get rid of the time dimension in _x values of tuples hold in TimeSeries
+        * and introduce the index of element instead
+        */
+      val timeSeriesValuesIndexed = ts.dataPoints.foldLeft[Seq[IndexValue]](Seq()) {
+        (x: Seq[IndexValue], y: (LocalDateTime, V)) => x :+ IndexValue(x.length, y._2)
       }
 
-      traverseTimeSeries[V](ts, 0, 0, false, Seq())
+      /**
+        * Create a sequence of samples, where every sample is a tuple of (preceding, succeeding)
+        */
+      val zippedPairs: Seq[(IndexValue, IndexValue)] = timeSeriesValuesIndexed.zip(timeSeriesValuesIndexed.tail)
+
+      zippedPairs.foldLeft[Seq[Session]](Seq()) {
+        (z: Seq[Session], e: (IndexValue, IndexValue)) => {
+          var result = z
+          val preceding = e._1
+          val succeeding = e._2
+
+          // definitely a corner case - but most often we start with this one:
+          // preceding value of the pair is non-zero and we
+          // did not detected any sessions so far - add a new Session element right now !
+          if ((preceding.value != 0) && (z.length == 0))
+            result = Seq(Session(preceding.index, succeeding.index))
+
+          // typical case - detected the start of session
+          if ((preceding.value == 0) && (succeeding.value != 0))
+            result = Session(succeeding.index, succeeding.index) +: z
+
+          // typical case - detected the end of session, and the session start index
+          // is passed in the head.startIndex value (since session start was previously detected)
+          if ((preceding.value != 0) && (succeeding.value == 0) && (z.length > 0))
+            result = Session(z.head.startIndex, succeeding.index-1) +: z.tail
+
+          // kind of a corner case - always update the end index, since e: (IndexValue, IndexValue)
+          // might be the last pair analysed !
+          if ((preceding.value != 0) && (succeeding.value != 0) && (z.length > 0))
+            result = Session(z.head.startIndex, succeeding.index) +: z.tail
+
+          result
+        }
+      }.reverse
     }
   }
 }
--- src/test/scala/carldata/series/AnalysisTest.scala
+++ src/test/scala/carldata/series/AnalysisTest.scala
@@ -7,37 +7,31 @@ class AnalysisTest extends FlatSpec with Matchers {
 
   "findSessions" should "detect sessions" in {
     val series: TimeSeries[Int] = TimeSeries.fromTimestamps(Seq((1, 4), (2, 6), (3, 5), (4, 3), (5, 0), (6, 4), (7, 6), (8, 3), (9, 1), (10, 0), (11, 7), (12, 8)))
-    val sessions = series.findSessions
-    sessions.length shouldBe 3
-    val firstDetected = sessions(0)
-    firstDetected.startIndex shouldBe 0
-    firstDetected.endIndex shouldBe 3
-    val secondDetected = sessions(1)
-    secondDetected.startIndex shouldBe 5
-    secondDetected.endIndex shouldBe 8
-    val thirdDetected = sessions(2)
-    thirdDetected.startIndex shouldBe 10
-    thirdDetected.endIndex shouldBe 11
+    val expected: Seq[Session] = Seq(Session(0, 3), Session(5, 8), Session(10, 11))
+    series.findSessions shouldBe expected
   }
 
   "findSessions" should "detect sessions distributed in consecutive zeros signal" in {
     val series: TimeSeries[Int] = TimeSeries.fromTimestamps(Seq((1, 0), (2, 0), (3, 0), (4, 1), (5, 1), (6, 0), (7, 0)))
-    val sessions = series.findSessions
-    sessions.length shouldBe 1
-    val firstDetected = sessions(0)
-    firstDetected.startIndex shouldBe 3
-    firstDetected.endIndex shouldBe 4
+    val expected: Seq[Session] = Seq(Session(3, 4))
+    series.findSessions shouldBe expected
   }
 
   "findSessions" should "detect sessions placed in the beginning and in the end of signal" in {
     val series: TimeSeries[Int] = TimeSeries.fromTimestamps(Seq((1, 1), (2, 1), (3, 0), (4, 1), (5, 0), (6, 1), (7, 1)))
-    val sessions = series.findSessions
-    sessions.length shouldBe 3
-    val firstDetected = sessions(0)
-    firstDetected.startIndex shouldBe 0
-    firstDetected.endIndex shouldBe 1
-    val thirdDetected = sessions(2)
-    thirdDetected.startIndex shouldBe 5
-    thirdDetected.endIndex shouldBe 6
+    val expected: Seq[Session] = Seq(Session(0, 1), Session(3, 3), Session(5, 6))
+    series.findSessions shouldBe expected
+  }
+
+  "findSessions" should "detect one session in signal of all non-zero values" in {
+    val series: TimeSeries[Int] = TimeSeries.fromTimestamps(Seq((1, 1), (2, 1), (3, 5), (4, 1), (5, 5), (6, 1), (7, 1)))
+    val expected: Seq[Session] = Seq(Session(0, 6))
+    series.findSessions shouldBe expected
+  }
+
+  "findSessions" should "detect no session in signal of all zero values" in {
+    val series: TimeSeries[Int] = TimeSeries.fromTimestamps(Seq((1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)))
+    val expected: Seq[Session] = Seq()
+    series.findSessions shouldBe expected
   }
 }
