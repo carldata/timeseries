b0c0a26d59a3297ba18867d9ae46a05127e84993
sharpcoding <sharp.coding@gmail.com>
2017-09-22 17:25:43 +0200

    next attempt to implement findSessions() - making the code much, much more succinct, getting rid of case class IndexValue(index: Int, value: V), using dropWhile() to trim the series initially

--- src/main/scala/carldata/series/Analysis.scala
+++ src/main/scala/carldata/series/Analysis.scala
@@ -1,57 +1,23 @@
 package carldata.series
 
-import java.time.LocalDateTime
-
-
 object Analysis {
   implicit class SeriesAnalysis[V](val ts: TimeSeries[V])(implicit num: Numeric[V]) {
 
     def findSessions: Seq[Session] = {
-      /**
-        * Auxiliary class for storing value of a TimeSeries and its associated index in TimeSeries (where it can be found)
-        */
-      case class IndexValue(index: Int, value: V)
-
-      /**
-        * First get rid of the time dimension in _x values of tuples hold in TimeSeries
-        * and introduce the index of element instead
-        */
-      val timeSeriesValuesIndexed = ts.dataPoints.foldLeft[Seq[IndexValue]](Seq()) {
-        (x: Seq[IndexValue], y: (LocalDateTime, V)) => x :+ IndexValue(x.length, y._2)
-      }
-
-      /**
-        * Create a sequence of samples, where every sample is a tuple of (preceding, succeeding)
-        */
-      val zippedPairs: Seq[(IndexValue, IndexValue)] = timeSeriesValuesIndexed.zip(timeSeriesValuesIndexed.tail)
-
-      zippedPairs.foldLeft[Seq[Session]](Seq()) {
-        (z: Seq[Session], e: (IndexValue, IndexValue)) => {
-          var result = z
-          val preceding = e._1
-          val succeeding = e._2
-
-          // definitely a corner case - but most often we start with this one:
-          // preceding value of the pair is non-zero and we
-          // did not detected any sessions so far - add a new Session element right now !
-          if ((preceding.value != 0) && (z.length == 0))
-            result = Seq(Session(preceding.index, succeeding.index))
-
-          // typical case - detected the start of session
-          if ((preceding.value == 0) && (succeeding.value != 0))
-            result = Session(succeeding.index, succeeding.index) +: z
-
-          // typical case - detected the end of session, and the session start index
-          // is passed in the head.startIndex value (since session start was previously detected)
-          if ((preceding.value != 0) && (succeeding.value == 0) && (z.length > 0))
-            result = Session(z.head.startIndex, succeeding.index-1) +: z.tail
-
-          // kind of a corner case - always update the end index, since e: (IndexValue, IndexValue)
-          // might be the last pair analysed !
-          if ((preceding.value != 0) && (succeeding.value != 0) && (z.length > 0))
-            result = Session(z.head.startIndex, succeeding.index) +: z.tail
-
-          result
+      val timeSeriesValuesIndexed = ts.values.zipWithIndex
+      val zippedPairs: Seq[((V, Int), (V, Int))] = timeSeriesValuesIndexed.zip(timeSeriesValuesIndexed.tail).dropWhile(x => x._1._1 == 0)
+      val zStart: Seq[Session] = if (zippedPairs.nonEmpty && zippedPairs.head._1._1 != 0)
+        Seq(Session(zippedPairs.head._1._2, zippedPairs.head._1._2))
+      else
+        Seq()
+      zippedPairs.foldLeft[Seq[Session]](zStart) {
+        (z: Seq[Session], e: ((V, Int), (V, Int))) => {
+          if ((e._1._1 == 0) && (e._2._1  != 0))
+            Session(e._2._2, e._2._2) +: z
+          else if (e._1._1 != 0)
+            Session(z.head.startIndex, e._2._2 + (if (e._2._1 != 0) 0 else -1)) +: z.tail
+          else
+            z
         }
       }.reverse
     }
