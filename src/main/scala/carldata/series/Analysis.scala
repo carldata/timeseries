184138a1a8d7eddb5e669e1addfaed1d861c98d1
sharpcoding <sharp.coding@gmail.com>
2017-09-25 11:12:20 +0200

    restructuring the algorithm

--- src/main/scala/carldata/series/Analysis.scala
+++ src/main/scala/carldata/series/Analysis.scala
@@ -4,14 +4,19 @@ object Analysis {
   implicit class SeriesAnalysis[V](val ts: TimeSeries[V])(implicit num: Numeric[V]) {
 
     def findSessions: Seq[Session] = {
-      val timeSeriesValuesIndexed = ts.values.zipWithIndex
-      val zippedPairs: Seq[((V, Int), (V, Int))] = timeSeriesValuesIndexed.zip(timeSeriesValuesIndexed.tail).dropWhile(x => x._1._1 == 0)
-      val zStart: Seq[Session] = if (zippedPairs.nonEmpty && zippedPairs.head._1._1 != 0)
-        Seq(Session(zippedPairs.head._1._2, zippedPairs.head._1._2))
+      val tss = ts.values.zipWithIndex //time series values indexed
+      val zps: Seq[((V, Int), (V, Int))] = tss.zip(tss.tail).dropWhile(x => x._1._1 == 0) //zipped pairs
+      val zStart: Seq[Session] = if (zps.nonEmpty && zps.head._1._1 != 0)
+        Seq(Session(zps.head._1._2, zps.head._1._2))
       else
         Seq()
-      zippedPairs.foldLeft[Seq[Session]](zStart) {
+      zps.foldLeft[Seq[Session]](zStart) {
         (z: Seq[Session], e: ((V, Int), (V, Int))) => {
+          // where:
+          //  e._1._1 - value of the first of compared pair (x, y)
+          //  e._2._1 - value of the second of compared pair (x, y)
+          //  e._1._2 - index of the first of compared pair (x, y)
+          //  e._2._2 - index of the second of compared pair (x, y)
           if ((e._1._1 == 0) && (e._2._1  != 0))
             Session(e._2._2, e._2._2) +: z
           else if (e._1._1 != 0)
