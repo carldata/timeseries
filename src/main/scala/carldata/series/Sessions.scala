4bc2fb0af371ffd787eb9bc37de7c748d240a497
sharpcoding <sharp.coding@gmail.com>
2017-09-28 15:40:38 +0200

    implementing faster version of findSessions() (with tolerance)

--- src/main/scala/carldata/series/Sessions.scala
+++ src/main/scala/carldata/series/Sessions.scala
@@ -26,24 +26,22 @@ object Sessions {
     }.reverse
   }
 
-  /** Finds a session with a duration tolerance
-    * @param maxTolerance the tolerance between two samples in minutes, for example:
-    *                       for a fragment of signal values distributed evenly for exactly one second
-    *                       (2, 0, 1, 0, 0, 2, 3, 4, 5, 7, 0)
-    *                       for Duration.ofSeconds(0) will be following sessions detected: [Session(0,0), Session(2,2), Session(5, 9)]
-    *                       for Duration.ofSeconds(1) will be following sessions detected: [Session(0,2), Session(5, 9)]
+  /** Finds sessions with a duration tolerance
+    * @param tolerance the time distance (inclusive) between two detected sessions that will make the sessions merged,
+    *                  for example, for a fragment of signal values distributed evenly for exactly one second
+    *                  (2, 0, 1, 0, 0, 2, 3, 4, 5, 7, 0)
+    *                  the Duration.ofSeconds(1) will be following sessions detected: [Session(0,0), Session(2,2), Session(5, 9)]
+    *                  the Duration.ofSeconds(2) will be following sessions detected: [Session(0,2), Session(5, 9)]
     * */
 
-  def findSessions[V: Numeric](ts: TimeSeries[V], maxTolerance: Duration)(implicit num: Fractional[V]): Seq[Session] = {
-    if (maxTolerance.compareTo(Duration.between(ts.head.get._1, ts.last.get._1)) >= 0)
+  def findSessions[V: Numeric](ts: TimeSeries[V], tolerance: Duration)(implicit num: Fractional[V]): Seq[Session] = {
+    if (tolerance.compareTo(Duration.between(ts.head.get._1, ts.last.get._1)) >= 0)
       Seq(Session(0, ts.length - 1))
     else {
       val xs = Sessions.findSessions(ts)
       val is = ts.index.zipWithIndex.map(x => (x._2, x._1))
-      val hs: immutable.Map[Int, LocalDateTime] = is.toMap
       xs.tail.foldLeft[Seq[Session]](Seq(xs.head))((zs, x) => {
-        if ((maxTolerance.compareTo(Duration.between(hs(zs.head.endIndex + 1), hs(x.startIndex))) >= 0) &&
-            (maxTolerance.compareTo(Duration.between(hs(zs.head.endIndex), hs(zs.head.endIndex + 1))) >= 0))
+        if (tolerance.compareTo(Duration.between(is(zs.head.endIndex)._2, is(x.startIndex)._2)) >= 0)
           Session(zs.head.startIndex, x.endIndex) +: zs.tail //merge sessions
         else
           x +: zs
--- src/test/scala/carldata/series/SessionsTest.scala
+++ src/test/scala/carldata/series/SessionsTest.scala
@@ -39,35 +39,6 @@ class SessionsTest extends FlatSpec with Matchers {
       |2008-01-01T13:03:00, 1
       |2008-01-01T13:04:00, 3""".stripMargin)
 
-  val seriesWithTimestampGaps = Csv.fromString(
-    """time,value
-      |2008-01-01T12:35:00, 4
-      |2008-01-01T12:36:00, 2
-      |2008-01-01T12:37:00, 3
-      |2008-01-01T12:38:00, 0
-      |2008-01-01T12:39:00, 0
-      |2008-01-01T12:40:00, 0
-      |2008-01-01T12:41:00, 0
-      |2008-01-01T12:42:00, 1
-      |2008-01-01T12:44:00, 0
-      |2008-01-01T12:45:00, 1
-      |2008-01-01T12:46:00, 2
-      |2008-01-01T12:48:00, 2
-      |2008-01-01T12:49:00, 5
-      |2008-01-01T12:50:00, 6
-      |2008-01-01T12:51:00, 7
-      |2008-01-01T12:52:00, 1
-      |2008-01-01T12:53:00, 0
-      |2008-01-01T12:54:00, 0
-      |2008-01-01T12:56:00, 0
-      |2008-01-01T12:57:00, 0
-      |2008-01-01T12:58:00, 2
-      |2008-01-01T12:59:00, 4
-      |2008-01-01T13:01:00, 0
-      |2008-01-01T13:02:00, 0
-      |2008-01-01T13:03:00, 1
-      |2008-01-01T13:04:00, 3""".stripMargin)
-
   val seriesOfEvanescentRain = Csv.fromString(
     """time,value
       |2008-01-01T12:35:00, 0
@@ -143,33 +114,18 @@ class SessionsTest extends FlatSpec with Matchers {
   }
 
   "findSessionsWithTolerance" should "detect sessions with toleration of 1 minute in typical rain signal" in {
-    val expected = Seq(Session(0, 2), Session(7, 17), Session(23, 24), Session(28, 29))
+    val expected = Seq(Session(0,2), Session(7,8), Session(10,17), Session(23,24), Session(28,29))
     Sessions.findSessions(seriesOfTypicalRain, Duration.ofMinutes(1)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 3 minutes in typical rain signal" in {
-    val expected = Seq(Session(0, 2), Session(7, 17), Session(23, 29))
+    val expected = Seq(Session(0,2), Session(7,17), Session(23,24), Session(28,29))
     Sessions.findSessions(seriesOfTypicalRain, Duration.ofMinutes(3)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 4 minutes in typical rain signal" in {
-    val expected = Seq(Session(0, 17), Session(23, 29))
+    val expected = Seq(Session(0,2), Session(7,17), Session(23,29))
     Sessions.findSessions(seriesOfTypicalRain, Duration.ofMinutes(4)) shouldBe expected
-  }
-
-  it should "detect sessions with toleration of 1 minute in signal having timestamp gaps" in {
-    val expected = Seq(Session(0, 2), Session(7, 7), Session(9, 15), Session(20, 21), Session(24, 25))
-    Sessions.findSessions(seriesWithTimestampGaps, Duration.ofMinutes(1)) shouldBe expected
-  }
-
-  it should "detect sessions with toleration of 3 minutes in signal having timestamp gaps" in {
-    val expected = Seq(Session(0, 2), Session(7, 15), Session(20, 25))
-    Sessions.findSessions(seriesWithTimestampGaps, Duration.ofMinutes(3)) shouldBe expected
-  }
-
-  it should "detect sessions with toleration of 4 minutes in signal having timestamp gaps" in {
-    val expected = Seq(Session(0, 15), Session(20, 25))
-    Sessions.findSessions(seriesWithTimestampGaps, Duration.ofMinutes(4)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 0 minutes in evanescent rain signal" in {
@@ -178,27 +134,27 @@ class SessionsTest extends FlatSpec with Matchers {
   }
 
   it should "detect sessions with toleration of 1 minute in evanescent rain signal" in {
-    val expected = Seq(Session(2,4), Session(7,7), Session(11,11), Session(16,16), Session(22,22))
+    val expected = Seq(Session(2,2), Session(4,4), Session(7,7), Session(11,11), Session(16,16), Session(22,22))
     Sessions.findSessions(seriesOfEvanescentRain, Duration.ofMinutes(1)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 2 minutes in evanescent rain signal" in {
-    val expected = Seq(Session(2,7), Session(11,11), Session(16,16), Session(22,22))
+    val expected = Seq(Session(2,4), Session(7,7), Session(11,11), Session(16,16), Session(22,22))
     Sessions.findSessions(seriesOfEvanescentRain, Duration.ofMinutes(2)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 3 minutes in evanescent rain signal" in {
-    val expected = Seq(Session(2,11), Session(16,16), Session(22,22))
+    val expected = Seq(Session(2,7), Session(11,11), Session(16,16), Session(22,22))
     Sessions.findSessions(seriesOfEvanescentRain, Duration.ofMinutes(3)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 4 minutes in evanescent rain signal" in {
-    val expected = Seq(Session(2,16), Session(22,22))
+    val expected = Seq(Session(2,11), Session(16,16), Session(22,22))
     Sessions.findSessions(seriesOfEvanescentRain, Duration.ofMinutes(4)) shouldBe expected
   }
 
   it should "detect sessions with toleration of 5 minutes in evanescent rain signal" in {
-    val expected = Seq(Session(2, 22))
+    val expected = Seq(Session(2,16), Session(22,22))
     Sessions.findSessions(seriesOfEvanescentRain, Duration.ofMinutes(5)) shouldBe expected
   }
 
