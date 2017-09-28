f33cade51f95c51407d6df613a0bb9f4e11d3fd3
Tomasz S-ki <sharp.coding@gmail.com>
2017-09-28 16:34:23 +0200

    implementing faster version of findSessions() (with tolerance) (#49)
    * implementing faster version of findSessions() (with tolerance)
    
    * Added session finder to the Benchmarks (#48)
    
    * implementing faster version of findSessions() (with tolerance)
    
    * implementing faster version of findSessions() (with tolerance)

--- src/main/scala/carldata/series/Sessions.scala
+++ src/main/scala/carldata/series/Sessions.scala
@@ -1,6 +1,8 @@
 package carldata.series
 
-import java.time.Duration
+import java.time.{Duration, LocalDateTime}
+
+import scala.collection.immutable
 
 object Sessions {
 
@@ -24,26 +26,21 @@ object Sessions {
     }.reverse
   }
 
-  /** Finds a session with a duration tolerance
-    * @param maxTolerance the tolerance understood as the maximal duration between two samples n, n+k,
-    *                     where k>0, one of them is allowed to be 0, as for example:
-    *                       for Duration.ofSeconds(1)
-    *                       for a fragment of signal values distributed evenly for exactly one second
-    *                       (2, 0, 1, 0, 0, 2, 3, 4, 5, 7, 0)
-    *                       there will be following sessions detected: [Session(0,2), Session(5, 9)]
+  /** Finds sessions with a duration tolerance
+    * @param tolerance the time distance (inclusive) between two detected sessions that will make the sessions merged,
+    *                  for example, for a fragment of signal values distributed evenly for exactly one second
+    *                  (2, 0, 1, 0, 0, 2, 3, 4, 5, 7, 0)
+    *                  the Duration.ofSeconds(1) will be following sessions detected: [Session(0,0), Session(2,2), Session(5, 9)]
+    *                  the Duration.ofSeconds(2) will be following sessions detected: [Session(0,2), Session(5, 9)]
     * */
-  def findSessions[V: Numeric](ts: TimeSeries[V], maxTolerance: Duration)(implicit num: Fractional[V]): Seq[Session] = {
-    if (maxTolerance.compareTo(Duration.between(ts.head.get._1, ts.last.get._1)) >= 0)
-      Seq(Session(0, ts.length - 1))
-    else {
-      val xs = ts.rollingWindow(maxTolerance, sx => sx.sum)
-      val ys = xs.map(x =>
-        if (xs.slice(x._1, x._1.plusMinutes(maxTolerance.toMinutes).plusNanos(1)).values.contains(0))
-          num.fromInt(0)
+
+  def findSessions[V: Numeric](ts: TimeSeries[V], tolerance: Duration)(implicit num: Fractional[V]): Seq[Session] = {
+      val xs = Sessions.findSessions(ts)
+      xs.tail.foldLeft[List[Session]](List(xs.head))((zs, x) => {
+        if (tolerance.compareTo(Duration.between(ts.index(zs.head.endIndex), ts.index(x.startIndex))) >= 0)
+          Session(zs.head.startIndex, x.endIndex) :: zs.tail //merge sessions
         else
-          x._2
-      )
-      Sessions.findSessions[V](ys)
-    }
+          x :: zs
+      }).reverse
   }
 }
