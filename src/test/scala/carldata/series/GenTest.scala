2a0e236ee9e405e6d1eb1a102a435a16d3709055
wenta <jakubwenta@gmail.com>
2018-01-24 12:39:32 +0100

    New repeat function. Fixed #77

--- src/test/scala/carldata/series/GenTest.scala
+++ src/test/scala/carldata/series/GenTest.scala
@@ -14,5 +14,29 @@ class GenTest extends FlatSpec with Matchers {
     result shouldBe expected
   }
 
+  "Repeat generator" should "create series when start date is before pattern." in {
+    val now = Instant.now()
+    val idx1 = Vector(now.plusSeconds(1), now.plusSeconds(2), now.plusSeconds(3), now.plusSeconds(4))
+    val idx2 = Vector(now.minusSeconds(2), now.minusSeconds(1), now,
+      now.plusSeconds(1), now.plusSeconds(2), now.plusSeconds(3), now.plusSeconds(4), now.plusSeconds(5))
+    val vs = Vector(1, 4, 6, 8)
+    val series = TimeSeries(idx1, vs)
+    val expected = TimeSeries(idx2, vs ++ vs)
+    val result = Gen.repeat(series, now.minusSeconds(2), now.plusSeconds(5))
+    result shouldBe expected
+  }
+
+  it should "create series when start date is after pattern." in {
+    val now = Instant.now()
+    val idx1 = Vector(now.plusSeconds(1), now.plusSeconds(2), now.plusSeconds(3), now.plusSeconds(4))
+    val idx2 = Vector(now.plusSeconds(11), now.plusSeconds(12), now.plusSeconds(13), now.plusSeconds(14),
+      now.plusSeconds(15), now.plusSeconds(16), now.plusSeconds(17), now.plusSeconds(18))
+    val vs = Vector(1, 4, 6, 8)
+    val series = TimeSeries(idx1, vs)
+    val expected = TimeSeries(idx2, vs ++ vs)
+    val result = Gen.repeat(series, now.plusSeconds(11), now.plusSeconds(18))
+    result shouldBe expected
+  }
+
 
 }
