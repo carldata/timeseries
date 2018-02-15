a8b702ae60461e3b0fcd6aeb41b8bfe9a811df97
Krzysztof Langner <klangner@gmail.com>
2018-02-15 15:27:39 +0100

    Better date time parser for CSV reader. Fixed #93

--- src/main/scala/carldata/series/Csv.scala
+++ src/main/scala/carldata/series/Csv.scala
@@ -1,15 +1,40 @@
 package carldata.series
 
 import java.time.{LocalDateTime, ZoneOffset}
-import java.time.format.DateTimeFormatter
+import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
+import java.time.temporal.ChronoField
 
 object Csv {
+
+  private val dateFormatter = new DateTimeFormatterBuilder()
+    .parseCaseInsensitive
+    .appendValue(ChronoField.YEAR)
+    .appendLiteral('-')
+    .appendValue(ChronoField.MONTH_OF_YEAR)
+    .appendLiteral('-')
+    .appendValue(ChronoField.DAY_OF_MONTH)
+    .optionalStart.appendLiteral(' ').optionalEnd
+    .optionalStart.appendLiteral('T').optionalEnd
+    .optionalStart
+    .appendValue(ChronoField.HOUR_OF_DAY)
+    .appendLiteral(':')
+    .appendValue(ChronoField.MINUTE_OF_HOUR)
+    .optionalStart.appendLiteral(':').appendValue(ChronoField.SECOND_OF_MINUTE).optionalEnd
+    .optionalStart.appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true).optionalEnd
+    .optionalStart.appendLiteral('Z').optionalEnd
+    .optionalEnd
+    .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+    .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+    .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+    .parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
+    .toFormatter
+
 
   /** Reader for CSV string */
   def fromString(str: String): TimeSeries[Double] = {
     val data = str.split("\n").tail.map{ line =>
       val tokens = line.split(",")
-      (LocalDateTime.parse(tokens(0)).toInstant(ZoneOffset.UTC), tokens(1).toDouble)
+      (LocalDateTime.parse(tokens(0), dateFormatter).toInstant(ZoneOffset.UTC), tokens(1).toDouble)
     }
     new TimeSeries(data)
   }
@@ -21,4 +46,5 @@ object Csv {
       .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "," + x._2.toString)
     header + "\n" + lines.mkString("\n")
   }
+
 }
--- src/test/scala/carldata/series/CsvTest.scala
+++ src/test/scala/carldata/series/CsvTest.scala
@@ -7,7 +7,7 @@ import org.scalatest._
 
 class CsvTest extends FlatSpec with Matchers {
 
-  "Csv" should "read series from string" in {
+  "Csv reader" should "read series from string" in {
     val str =
       """time,value
         |2005-01-01T12:34:15,2
@@ -18,7 +18,29 @@ class CsvTest extends FlatSpec with Matchers {
     series.values shouldBe Vector(2, -4, -6, 9)
   }
 
-  it should "save series to string" in {
+  it should "read custom time format" in {
+    val str =
+      """time,value
+        |2005-01-01 12:34:15,2
+        |2006-01-01 12:34:15,-4
+        |2007-01-01 12:34:15,-6
+        |2008-01-01 12:34:15,9""".stripMargin
+    val series = Csv.fromString(str)
+    series.values shouldBe Vector(2, -4, -6, 9)
+  }
+
+  it should "read custom time format 2" in {
+    val str =
+      """time,value
+        |2005-01-01,2
+        |2006-01-01,-4
+        |2007-01-01,-6
+        |2008-01-01,9""".stripMargin
+    val series = Csv.fromString(str)
+    series.values shouldBe Vector(2, -4, -6, 9)
+  }
+
+  "CSV Writer" should "save series to string" in {
     val now = Instant.ofEpochSecond(1000)
     val idx = Vector(now, now.plusSeconds(1), now.plusSeconds(2), now.plusSeconds(3))
     val series = TimeSeries(idx, Vector(1f, 4f, 6f, 9f))
