d2acba90a1a53cf5eecccc9a7a7537086c546709
wenta <jakubwenta@gmail.com>
2018-02-19 12:23:39 +0100

    Estimate pattern #91 (#95)
    * Estimate pattern #91
    
    * Refactor. Allow to group with any time

---
+++ src/main/scala/carldata/series/Patterns.scala
@@ -0,0 +1,24 @@
+package carldata.series
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+object Patterns {
+  def daily[V](xs: TimeSeries[V])(implicit num: Fractional[V]): TimeSeries[V] = {
+    /** Calculate seconds passed from midnight **/
+    def fromMidnight(dt: Instant): Long = {
+      dt.getEpochSecond - dt.truncatedTo(ChronoUnit.DAYS).getEpochSecond
+    }
+
+    def groupByDuration(dt: Instant): Instant = {
+      Instant.EPOCH
+        .truncatedTo(ChronoUnit.DAYS)
+        .plusSeconds(fromMidnight(dt))
+    }
+
+    def average(vs: Seq[(Instant, V)]): V = num.div(vs.unzip._2.sum, num.fromInt(vs.length))
+
+    xs.groupByTime(groupByDuration, average)
+  }
+
+}
---
+++ src/test/scala/carldata/series/PatternsTest.scala
@@ -0,0 +1,19 @@
+package carldata.series
+
+import java.time.Instant
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class PatternsTest extends FlatSpec with Matchers {
+  "Patterns" should "estimate daily pattern" in {
+    val now = Instant.EPOCH
+    val day = 86400
+    val idx = Vector(now, now.plusSeconds(300), now.plusSeconds(600)
+      , now.plusSeconds(day), now.plusSeconds(day + 300), now.plusSeconds(day + 600)
+      , now.plusSeconds(2 * day), now.plusSeconds(2 * day + 300), now.plusSeconds(2 * day + 600))
+    val series = TimeSeries(idx, Vector(1d, 2d, 3d, 2d, 4d, 6d, 3d, 6d, 9d))
+    val expected = TimeSeries(Vector(now, now.plusSeconds(300), now.plusSeconds(600)), Vector(2d, 4d, 6d))
+    val result = Patterns.daily(series)
+    result shouldBe expected
+  }
+}
