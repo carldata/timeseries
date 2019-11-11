286335884c47eb344c4e021521922154add4df92
wenta <jakubwenta@gmail.com>
2019-11-11 21:08:06 +0100

    Add locf to fill missing data. Fixed #133

--- shared/src/main/scala/carldata/extras/MissingDataModule.scala
+++ shared/src/main/scala/carldata/extras/MissingDataModule.scala
@@ -2,12 +2,16 @@ package carldata.extras
 
 import java.time.{Duration, Instant}
 
-import carldata.series.TimeSeries
+import carldata.series.{Gen, TimeSeries}
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object MissingDataModule {
 
   implicit class MissingDataTimeSeriesOperations[V](ts: TimeSeries[V]) {
     /** Filling missing data with last known value
+      *
       * @param delta Expected distance between points
       */
     def forwardFill(delta: Duration = ts.resolution): TimeSeries[V] = {
@@ -15,13 +19,54 @@ object MissingDataModule {
 
       ts.addMissing(ts.resolution, f)
     }
+
     /** Filling missing data with next known value
+      *
       * @param delta Expected distance between points
       */
     def backwardFill(delta: Duration = ts.resolution): TimeSeries[V] = {
       def f(x1: (Instant, V), x2: (Instant, V), tsh: Instant): V = x2._2
 
       ts.addMissing(ts.resolution, f)
+    }
+
+    /** Filling missing data with last known value.
+      * Last observation carried forward.
+      *
+      * @param maxGap size of space (of missing points) after which we decide to drop filling missing points
+      *               (i.e. inserting measurement from last month may have no sense).
+      */
+    def locf(maxGap: Duration = Duration.ofDays(365))(implicit num: Fractional[V]): TimeSeries[V] = {
+      if (ts.isEmpty) ts
+      else {
+        val resampledIndex = Gen.mkIndex(ts.index.head, ts.index.last, ts.resolution)
+
+        @tailrec def g(idxs: Vector[Instant], xs: Vector[(Instant, V)]
+                       , prev: (Instant, V), res: ListBuffer[(Instant, V)]): ListBuffer[(Instant, V)] = {
+          if (xs.nonEmpty) {
+            val xsh = xs.head
+            if (idxs.isEmpty) {
+              g(idxs, xs.tail, xsh, res :+ xs.head)
+            } else if (xsh._1 == idxs.head) {
+              g(idxs.tail, xs.tail, xsh, res :+ xs.head)
+            }
+            else if (idxs.head.isBefore(xsh._1)) {
+              if (prev._1.plus(maxGap).isAfter(xsh._1.minusNanos(1))) {
+                val dp = (idxs.head, prev._2)
+                g(idxs.tail, xs, dp, res :+ dp)
+              }
+              else g(idxs.tail, xs, prev, res)
+            }
+            else g(idxs, xs.tail, xsh, res :+ xs.head)
+          }
+          else res
+        }
+
+
+        val dp = ts.dataPoints
+        val res = g(resampledIndex, dp, dp.head, ListBuffer())
+        new TimeSeries(res)
+      }
     }
   }
 
